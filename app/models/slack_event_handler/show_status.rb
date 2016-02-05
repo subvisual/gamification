1ed46235653f1b951d734a3dd9d2bd39df7a8a3a
Gabriel Poça <gabrielpoca@gmail.com>
2016-02-05 18:51:48 +0000

    Show status

--- app/models/slack_event_handler/show_status.rb
+++ app/models/slack_event_handler/show_status.rb
@@ -7,16 +7,14 @@ class SlackEventHandler
     matcher Matchers::HasRegex, regex: /status/
 
     def run
-      puts @event
-      puts status
-      client.send({ type: status, text: "Trinta", channel: event.channel })
+      client.send(type: "message", text: status, channel: event.channel)
     end
 
     private
 
     def status
-      User.all.map do |user|
-        "#{user.name} has #{user.points}" 
+      User.order(points: :desc).all.map do |user|
+        "#{user.name} has #{user.points} points"
       end.join("\n")
     end
   end
