7212b50cb5c9ce6ce88c151b478eca4a15b662a9
João Justo <jpjustonunes@gmail.com>
2016-02-05 16:50:41 +0000

    Dashboard - it's something

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ActiveRecord::Base
   include Clearance::User
 
   has_many :events
+
+  def first_name
+    name.split.first
+  end
+
+  def position
+    '1st'
+  end
+
+  def titles
+    'O cêpo'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     DashboardManifest::DASHBOARDS.each do |dashboard_resource|
       resources dashboard_resource
     end
-
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
   end
+
+  root 'dashboard#index'
 end
