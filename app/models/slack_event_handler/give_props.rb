219040adca0715c627fb3bd203967afe493100ff
Miguel Palhas <mpalhas@gmail.com>
2016-02-05 22:17:50 +0000

    WIP [ci skip]

--- app/models/slack_event_handler/give_props.rb
+++ app/models/slack_event_handler/give_props.rb
@@ -9,14 +9,9 @@ class SlackEventHandler
 
     def run
       receiver = User.find_by_slack_id(event.mentions.first)
-      props_action = Action.where(slug: :props).first
-
       return unless receiver
 
-      ActiveRecord::Base.transaction do
-        receiver.events.create(action: props_action, points: props_action.points)
-        receiver.increment!(:points, props_action.points)
-      end
+      EventCreator.new(action_slug: :props, user: receiver)
       client.send(text: "Quarenta!", channel: event.channel, type: "message")
     end
   end
